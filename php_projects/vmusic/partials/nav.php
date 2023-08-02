46eb2f6e0687c6204729cd19175d8fa598f3246e
Vraj Patel <86770796+iamvrajpatel@users.noreply.github.com>
2023-08-02 23:31:22 +0530

    Add files via upload

---
+++ php_projects/vmusic/partials/nav.php
@@ -0,0 +1,19 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="stylesheet" href="./nav.css">
+
+<ul class="nav nav-tabs" style=" background-color:lightgoldenrodyellow">
+    <li class="nav-item">
+        <a class="nav-link" href="login.php" style="color: black">Login</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="create.php" style="color: black">Sign Up</a>
+    </li>
+    <!-- <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+        </li> -->
+</ul>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
